befb1ecdb4957617012b14a5a978a9fe22d61c49
oumaima <oumaimaabouelhaitam@gmail.com>
2022-11-15 20:29:56 +0100

    add function logout

--- frontend/src/components/Authentification/Header.jsx
+++ frontend/src/components/Authentification/Header.jsx
@@ -1,36 +1,51 @@
- import {Link} from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
 
-
-function Header(){
-
-    return(
-
-       
-          <>
-            <nav className="relative flex flex-wrap items-center justify-between px-0 py-3 btncolor mb-3">
-              <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-                <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-                  <span
-                    className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-                  >
-                   Livraison Marhaba
-                  </span>
-                 
-                </div>
+function Header() {
+  const navigate = useNavigate();
+  function logOut() {
+    localStorage.clear();
+    navigate("/login");
+  }
+  return (
+    <>
+      <nav className="relative flex flex-wrap items-center justify-between px-0 py-3 btncolor mb-3">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <span className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white">
+              Livraison Marhaba
+            </span>
+          </div>
+          <div>
+            <nav className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              {localStorage.getItem("token") ? (
+                <button
+                  className="fab fa-pinterest text-lg leading-lg text-white "
+                  onClick={logOut}
+                >
+                  logout
+                </button>
+              ) : (
                 <div>
-
-                  <nav className="flex flex-col lg:flex-row list-none lg:ml-auto">
-                 <Link to="/login" className="fab fa-twitter text-lg leading-lg text-white mx-2">login</Link>     
-                  <Link to="/register"  className="fab fa-pinterest text-lg leading-lg text-white ">register</Link>
-                    </nav>
-
-
+                  <Link
+                    to="/login"
+                    className="fab fa-twitter text-lg leading-lg text-white mx-2"
+                  >
+                    login
+                  </Link>
+                  <Link
+                    to="/register"
+                    className="fab fa-pinterest text-lg leading-lg text-white mx-2 "
+                  >
+                    register
+                  </Link>
                 </div>
-              </div>
+              )}
             </nav>
-          </>
-       
-    )
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Header;
--- frontend/src/components/Authentification/home.jsx
+++ frontend/src/components/Authentification/home.jsx
@@ -13,22 +13,7 @@ function Home() {
   return (
     <section class="mt-20 w-3/12 mx-auto bg-[#20354b] rounded-2xl px-8 py-6 shadow-lg">
       <div className="flex items-center justify-between">
-        <span className="text-emerald-400">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-            />
-          </svg>
-        </span>
+        
       </div>
       <div className="mt-6 w-fit mx-auto">
         <img
